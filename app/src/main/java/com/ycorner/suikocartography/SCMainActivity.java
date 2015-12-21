b1ae361e51a723acc326446fb9c58bd11f18b4dc
Michael Huh <michael@shuddle.us>
2015-12-21 13:57:37 -0800

    * SCConstants: This class will contain constant variables used throughout the application. * SCGameUtility: This class contains ENUM variables that will be used throughout the application. * SCMainActivity: Added launchMapsIntent and initButton() methods.

--- app/src/main/java/com/ycorner/suikocartography/SCMainActivity.java
+++ app/src/main/java/com/ycorner/suikocartography/SCMainActivity.java
@@ -1,16 +1,23 @@
 package com.ycorner.suikocartography;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import com.huhx0015.hxgselib.audio.HXGSEMusicEngine;
 import com.huhx0015.hxgselib.audio.HXGSESoundHandler;
-
+import com.ycorner.suikocartography.utility.SCConstants;
+import com.ycorner.suikocartography.utility.SCGameUtility;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class SCMainActivity extends Activity {
 
     private HXGSEMusicEngine musicEngine;
     private HXGSESoundHandler soundHandler;
+
+    @Bind(R.id.sc_suikoden_1_maps_button) Button suikoden_1_maps_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +30,51 @@ public class SCMainActivity extends Activity {
     private void initLayout() {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        initButtons();
+    }
+
+    private void initButtons() {
+
+        suikoden_1_maps_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchMapsIntent(SCGameUtility.SCGameID.GENSO_SUIKODEN_1);
+            }
+        });
     }
 
     private void initAudio() {
         musicEngine.getInstance().initializeAudio(this);
         soundHandler.getInstance().initializeAudio(this, 2);
+    }
+
+    private void launchMapsIntent(SCGameUtility.SCGameID id) {
+
+        String extraGameIdentifier = "";
+
+        switch (id) {
+            case GENSO_SUIKODEN_1:
+                extraGameIdentifier = SCConstants.GENSO_SUIKODEN_1_ID;
+                break;
+            case GENSO_SUIKODEN_2:
+                extraGameIdentifier = SCConstants.GENSO_SUIKODEN_2_ID;
+                break;
+            case GENSO_SUIKODEN_3:
+                extraGameIdentifier = SCConstants.GENSO_SUIKODEN_3_ID;
+                break;
+            case GENSO_SUIKODEN_4:
+                extraGameIdentifier = SCConstants.GENSO_SUIKODEN_4_ID;
+                break;
+            case GENSO_SUIKODEN_5:
+                extraGameIdentifier = SCConstants.GENSO_SUIKODEN_5_ID;
+                break;
+        }
+
+        if (!extraGameIdentifier.isEmpty()) {
+            Intent mapsIntent = new Intent(this, SCMapActivity.class);
+            mapsIntent.putExtra(SCConstants.EXTRA_GAME_NAME, extraGameIdentifier);
+            startActivity(mapsIntent);
+        }
     }
 }
---
+++ app/src/main/java/com/ycorner/suikocartography/utility/SCGameUtility.java
@@ -0,0 +1,17 @@
+package com.ycorner.suikocartography.utility;
+
+import android.content.Intent;
+
+/**
+ * Created by huhx0015 on 12/21/15.
+ */
+public class SCGameUtility {
+
+    public enum SCGameID {
+        GENSO_SUIKODEN_1,
+        GENSO_SUIKODEN_2,
+        GENSO_SUIKODEN_3,
+        GENSO_SUIKODEN_4,
+        GENSO_SUIKODEN_5;
+    }
+}
