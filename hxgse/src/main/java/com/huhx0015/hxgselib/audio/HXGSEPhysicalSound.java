104b7768e6c6ca8a9cdd0577c229376804f1a924
huhx0015 <huhx0015@gmail.com>
2016-12-29 10:58:46 -0800

    Updated ByakuGallery and HXGSE modules, updated dependencies, updated target SDK to 25.

--- hxgse/src/main/java/com/huhx0015/hxgselib/audio/HXGSEPhysicalSound.java
+++ hxgse/src/main/java/com/huhx0015/hxgselib/audio/HXGSEPhysicalSound.java
@@ -16,20 +16,29 @@ public class HXGSEPhysicalSound {
     /** CLASS FUNCTIONALITY ____________________________________________________________________ **/
 
     // disablePhysicalSounds(): Enables or disables the devices physical button's sound effects.
+    // Note: Android 7.0 and above requires "android.permission.ACCESS_NOTIFICATION_POLICY".
     public static void disablePhysSounds(boolean mode, Context context) {
 
-        AudioManager mgr = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        // ANDROID 2.3 - ANDROID 6.0:
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            AudioManager mgr = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 
-            if (mode) {
-                mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_MUTE, 0);
-            } else {
-                mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_UNMUTE, 0);
+            // ANDROID 6.0+:
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                if (mode) {
+                    mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_MUTE, 0);
+                } else {
+                    mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_UNMUTE, 0);
+                }
+
             }
 
-        } else {
-            mgr.setStreamMute(AudioManager.STREAM_SYSTEM, mode);
+            // ANDROID 2.3 - ANDROID 5.1:
+            else {
+                mgr.setStreamMute(AudioManager.STREAM_SYSTEM, mode);
+            }
         }
     }
 }
